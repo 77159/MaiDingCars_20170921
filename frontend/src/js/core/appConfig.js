b1df1107fa6dc5b9069c8077f3bebc5ba02fbab5
77159 <77159202@qq.com>
2017-11-03 18:03:43 +0800

    git upload test

--- frontend/src/js/core/appConfig.js
+++ frontend/src/js/core/appConfig.js
@@ -12,7 +12,6 @@ export const AppConfig = {
     //后台接口服务地址
     // serviceUrl: 'http://192.168.1.95:9095/',
     // serviceUrl: 'http://192.168.1.72:9090/',
-    // git upload test
     serviceUrl: 'http://192.168.1.168:8090/',
     get token() {
         //let token = cookies.getItem(this.fmTokenCookieName);
